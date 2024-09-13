ebd2e9d3a71bcead59d7e0e5bab588b2bcfba67c
mohamed <muhammedzain1504@gmail.com>
2024-09-13 21:49:57 +0300

    Home page ui search page ui

--- lib/moduls/search/pages/search_page.dart
+++ lib/moduls/search/pages/search_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
+
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("search Page")),
+    return SafeArea(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+            /*height: 100,*/
+            child: TextFormField(
+              onChanged: (String? value) {},
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                filled: true,
+                fillColor: Color.fromRGBO(81, 79, 79, 1.0),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                enabled: true,
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                  size: 24,
+                ),
+                hintText: 'search',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
