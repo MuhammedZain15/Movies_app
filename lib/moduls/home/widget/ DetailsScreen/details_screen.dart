1bfb45a0890a39fca626339395bca37614339010
mostafamagde <mostafamagde228@gmail.com>
2024-09-27 02:21:28 +0300

    fix error incorrect parentles

--- lib/moduls/home/widget/ DetailsScreen/details_screen.dart
+++ lib/moduls/home/widget/ DetailsScreen/details_screen.dart
@@ -51,159 +51,155 @@ class DetailsScreen extends StatelessWidget {
                   ),
                   backgroundColor: theme.primaryColor,
                 ),
-                body: Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          width: double.infinity,
-                          height: size.height * 0.21,
-                          child: Stack(
-                            children: [
-                              Image.network(
-                                "${Constants.link}${cubit.movieDetails?.backDropImage}",
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                              ),
-                              Positioned(
-                                left: size.width * 0.40,
-                                top: size.height * 0.08,
-                                child:  Icon(
-                                  Icons.play_circle_filled,
-                                  size: 70.sp,
-                                  color: Colors.white,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                         SizedBox(
-                          height: 10.h,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                width: size.width * .94,
-                                child: Text(
-                                  cubit.movieDetails!.movieTitle,
-                                  overflow: TextOverflow.visible,
-                                  maxLines: 2,
-                                  style:  TextStyle(
-                                    fontSize: 16.sp,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: Row(
-                            children: [
-                              Text(
-                                cubit.movieDetails!.releaseDate,
-                                style: TextStyle(
-                                  fontSize: 13.sp,
-                                  color: Color.fromRGBO(181, 180, 180, 1.0),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8.h,
-                        ),
-                        Row(
+                body: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: size.height * 0.21,
+                        child: Stack(
                           children: [
-                            Padding(
-                              padding: EdgeInsets.only(left: 10),
-                              child: ItemPhoto(path: cubit.movieDetails!.posterImage),
+                            Image.network(
+                              "${Constants.link}${cubit.movieDetails?.backDropImage}",
+                              fit: BoxFit.cover,
+                              width: double.infinity,
                             ),
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 75.h,
-                                  width: size.width * .3,
-                                  child: GridView.builder(
-                                    padding:
-                                        const EdgeInsets.symmetric(horizontal: 10),
-                                    itemCount: cubit.movieDetails!.genreIds.length,
-                                    scrollDirection: Axis.horizontal,
-                                    gridDelegate:
-                                        const SliverGridDelegateWithFixedCrossAxisCount(
-                                            crossAxisCount: 2,
-                                            mainAxisSpacing: 5,
-                                            crossAxisSpacing:1,
-                                            mainAxisExtent: 60),
-                                    itemBuilder: (BuildContext context, int index) {
-                                      return CategoryContainer(
-                                        category: cubit
-                                            .movieDetails!.genreIds[index].name,
-                                      );
-                                    },
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 10.h,
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 10),
-                                  child: SizedBox(
-                                    width: size.width * .64,
-                                    child: Text(
-                                      cubit.movieDetails!.overview,
-                                      maxLines: 5,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 13.sp,
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 10.h,
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 7),
-                                  child: Row(
-                                    children: [
-                                       Icon(
-                                        Icons.star_rounded,
-                                        color: Color(0xFFFFBB3B),
-                                        size: 30.sp,
-                                      ),
-                                       SizedBox(
-                                        width: 5.w,
-                                      ),
-                                      Text(
-                                        cubit.movieDetails!.rating
-                                            .toString()
-                                            .substring(0, 3),
-                                        style:  TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 20.sp,
-                                            fontWeight: FontWeight.w400),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
+                            Positioned(
+                              left: size.width * 0.40,
+                              top: size.height * 0.08,
+                              child:  Icon(
+                                Icons.play_circle_filled,
+                                size: 70.sp,
+                                color: Colors.white,
+                              ),
                             )
                           ],
                         ),
-                        Builder(builder: (context) {
-                          return MoreMoviesWidget(
-                            movie: cubit.movies,
-                          );
-                        }),
-                      ],
-                    ),
+                      ),
+                       SizedBox(
+                        height: 10.h,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: size.width * .94,
+                              child: Text(
+                                cubit.movieDetails!.movieTitle,
+                                overflow: TextOverflow.visible,
+                                maxLines: 2,
+                                style:  TextStyle(
+                                  fontSize: 16.sp,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          children: [
+                            Text(
+                              cubit.movieDetails!.releaseDate,
+                              style: TextStyle(
+                                fontSize: 13.sp,
+                                color: Color.fromRGBO(181, 180, 180, 1.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.h,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: ItemPhoto(path: cubit.movieDetails!.posterImage),
+                          ),
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 75.h,
+                                width: size.width * .3,
+                                child: GridView.builder(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 10),
+                                  itemCount: cubit.movieDetails!.genreIds.length,
+                                  scrollDirection: Axis.horizontal,
+                                  gridDelegate:
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount: 2,
+                                          mainAxisSpacing: 5,
+                                          crossAxisSpacing:1,
+                                          mainAxisExtent: 60),
+                                  itemBuilder: (BuildContext context, int index) {
+                                    return CategoryContainer(
+                                      category: cubit
+                                          .movieDetails!.genreIds[index].name,
+                                    );
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.h,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 10),
+                                child: SizedBox(
+                                  width: size.width * .64,
+                                  child: Text(
+                                    cubit.movieDetails!.overview,
+                                    maxLines: 5,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 13.sp,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.h,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 7),
+                                child: Row(
+                                  children: [
+                                     Icon(
+                                      Icons.star_rounded,
+                                      color: Color(0xFFFFBB3B),
+                                      size: 30.sp,
+                                    ),
+                                     SizedBox(
+                                      width: 5.w,
+                                    ),
+                                    Text(
+                                      cubit.movieDetails!.rating
+                                          .toString()
+                                          .substring(0, 3),
+                                      style:  TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20.sp,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      MoreMoviesWidget(
+                        movie: cubit.movies,
+                      ),
+                    ],
                   ),
                 ),
               );
