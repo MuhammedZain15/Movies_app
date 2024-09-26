46479ac2f150067d03c8bffdd97041cdcddfe09a
mostafamagde <mostafamagde228@gmail.com>
2024-09-27 02:12:43 +0300

    firebase config added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies_app/layout/layout_view.dart';
-import 'package:movies_app/moduls/browse/widget/browse_list.dart';
 import 'package:movies_app/moduls/home/widget/%20DetailsScreen/details_screen.dart';
 import 'package:movies_app/moduls/splash/splash_view.dart';
-import 'package:movies_app/services/api_manager/apimanager.dart';
 import 'package:movies_app/services/theme/application_theme_manger.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+
+
+WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
